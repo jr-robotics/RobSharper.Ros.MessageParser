4689b17be397e8c092d783cb52988bb40af2f9ac
Guido Breitenhuber <guido.breitenhuber@joanneum.at>
2019-04-23 14:42:33 +0200

    Add Examples project

---
+++ Joanneum.Robotics.Ros.MessageParser.Examples/ParseMessageFileExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Joanneum.Robotics.Ros.MessageParser.Examples
+{
+    public class ParseMessageFileExample
+    {
+        public void ParseFileFContent(string filePath)
+        {
+            var descriptor = MessageParser.Parse(File.ReadAllText(filePath));
+        }
+        
+        public void ParseFileStream(string filePath)
+        {
+            MessageDescriptor descriptor;
+            
+            using (var file = File.OpenRead(filePath))
+            {
+                descriptor = MessageParser.Parse(file);
+            }
+        }
+    }
+}
